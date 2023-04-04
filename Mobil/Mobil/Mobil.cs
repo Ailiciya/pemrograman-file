c789bbd4b4e1e5bcd1883e75113e0b1b14dfc61c
Kevin <114798157+KevinKyun@users.noreply.github.com>
2023-04-04 17:21:38 +0700

    Add files via upload

---
+++ Mobil/Mobil/Mobil.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pert5
+{
+    public class Mobil
+    {
+        private string merek;
+        private int tahun;
+
+        // Constructor 1
+
+        public Mobil(string m, int t)
+        {
+            merek = m;
+            tahun = t;
+        }
+        // Constructor 2
+
+        public Mobil()
+        {
+            merek = "Belum diatur";
+            tahun = 0;
+        }
+        public void Info()
+        {
+            Console.WriteLine("Mobil {0} tahun {1}", merek, tahun);
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Mobil/Mobil/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pert5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mobil mobil1 = new Mobil("Toyota", 2000); // ada parameter input.
+            Mobil mobil2 = new Mobil(); // tidak ada parameter input.
+            mobil1.Info();
+            mobil2.Info();
+        }
+    }
+}
