38f3ca99b6219f05344b55f0adccee19cea15f89
Kevin <114798157+KevinKyun@users.noreply.github.com>
2023-04-04 19:51:55 +0700

    Add files via upload

---
+++ NonVoid/NonVoid/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pert5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Penjumlahan(int a , int b)
+            {
+                return a + b;
+            }
+            int hasil = Penjumlahan(2, 3);
+            Console.WriteLine(hasil);
+            Console.ReadKey();
+        }
+    }
+}
