db796d26296dcc174aac84f87cd566d69347c6b7
Kevin <114798157+KevinKyun@users.noreply.github.com>
2023-04-04 19:47:29 +0700

    Add files via upload

---
+++ LatihanOverloading1/LatihanOverloading1/Overloading.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanOverloading1
+{
+    internal class Overloading
+    {
+        public int CariNilaiMin(int a, int b)
+        {
+            int min;
+            if (a < b)
+            {
+                Console.WriteLine("Nilai min dari kedua nilai adalah : {0}", min = a);
+                Console.ReadKey();
+            }
+            else 
+            {
+                Console.WriteLine("Nilai min dari kedua nilai adalah : {0}", min = b);
+                Console.ReadLine();
+            }
+                return min;
+
+        }
+        public int CariNilaiMax(int a, int b)
+        {
+            int max;
+            if (a > b)
+            {
+                Console.WriteLine("Nilai max dari kedua nilai adalah: {0}", max = a);
+            }
+            else
+            {
+                Console.WriteLine("Nilai max dari kedua nilai adalah: {0}", max = b);
+            }
+
+            return max;
+        }
+    }
+}
---
+++ LatihanOverloading1/LatihanOverloading1/Program.cs
@@ -0,0 +1,25 @@
+﻿using LatihanOverloading1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanOveloading1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Overloading overloading1 = new Overloading();
+
+            int a = 236549;
+            int b = 12523;
+
+            int nilaiMin = overloading1.CariNilaiMin(a, b);
+            int nilaiMax = overloading1.CariNilaiMax(a, b);
+
+            Console.ReadLine();
+        }
+    }
+}
