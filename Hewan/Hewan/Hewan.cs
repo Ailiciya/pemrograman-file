5626f0db8c1a59aad20f17f31ed25b7afcc1843d
Kevin <114798157+KevinKyun@users.noreply.github.com>
2023-04-04 14:47:30 +0700

    Add files via upload

---
+++ Hewan/Hewan/Hewan.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hewan
+{
+    public class Hewan
+    {
+        public virtual void Makan()
+        {
+            Console.WriteLine("Hewan sedang makan Nasi padang");
+            Console.ReadLine();
+        }
+    }
+    public class Kucing:Hewan
+    {
+        public override void Makan()
+        {
+            Console.WriteLine("Kucing Sedang Makan Nasi padang");
+            Console.ReadLine ();
+        }
+    }
+}
---
+++ Hewan/Hewan/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hewan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hewan hewan = new Hewan(); //membuat objek dari class hewan.
+            Kucing kucing = new Kucing(); // membuat objek dari class turunan kucing.
+
+            //pemanggilan method.
+
+            hewan.Makan();
+            kucing.Makan();
+        }
+    }
+}
