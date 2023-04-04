f588cdbf506ef8108dda16109352765d53a1fdd1
Kevin <114798157+KevinKyun@users.noreply.github.com>
2023-04-04 14:46:26 +0700

    Add files via upload

---
+++ ClassMember2/ClassMember2/Program.cs
@@ -0,0 +1,22 @@
+﻿using ClassMember2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pert5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hasil = Calculator.Penjumlahan(2, 3);
+            Console.WriteLine(hasil);
+            Console.ReadLine();
+            
+               
+            
+        }
+    }
+}
