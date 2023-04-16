191c6c9a92437a16f5a9ded54ff20c0696f58c73
Kevin <114798157+KevinKyun@users.noreply.github.com>
2023-04-16 20:26:34 +0700

    Add files via upload

---
+++ Encapsulation/Encapsulation/BMW.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    internal class BMW
+    {
+        private string Namamodel;
+        private string warna;
+        private int harga;
+        private static int jumlahMobil = 0;
+
+        public BMW(string namamodel, string warna, int harga)
+        {
+            Namamodel = namamodel;
+            this.warna = warna;
+            this.harga = harga;
+            jumlahMobil++;
+        }
+
+        public string getNamamodel()
+        {
+            return Namamodel;
+        }
+
+        public string getwarna()
+        {
+            return warna;
+        }
+
+        public int getHarga()
+        {
+            return harga;
+        }
+
+        public static int getJumlahMobil()
+        {
+            return jumlahMobil;
+        }
+    }
+}
---
+++ Encapsulation/Encapsulation/Program.cs
@@ -0,0 +1,20 @@
+﻿using Encapsulation;
+
+public class main
+{
+    public static void Main(string[] args)
+    {
+        BMW mobil1 = new BMW("iX3", "Hitam", 85095);
+        BMW mobil2 = new BMW("i4", "Putih", 51400);
+        
+        Console.WriteLine("Mobil 1:");
+        Console.WriteLine("Model: " + mobil1.getNamamodel());
+        Console.WriteLine("Warna: " + mobil1.getwarna());
+        Console.WriteLine("Harga: " + mobil1.getHarga());
+
+        Console.WriteLine("\nMobil 2: ");
+        Console.WriteLine("Model: " + mobil2.getNamamodel());
+        Console.WriteLine("Warna: " + mobil2.getwarna());
+        Console.WriteLine("Harga: " + mobil2.getHarga());
+    }
+}
