f046caad8402fe955e52aa5ed16c66364e3f46a7
Kevin <114798157+KevinKyun@users.noreply.github.com>
2023-04-16 23:14:01 +0700

    Add files via upload

---
+++ Constructor/Constructor/Mahasiswa.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    internal class Mahasiswa
+    {
+        // field
+        public string nama;
+        public string nim;
+        public string asal;
+
+        //constructor
+        public Mahasiswa(string nama, string nim, string asal) 
+        {
+        this.nama = nama;
+        this.nim = nim;
+        this.asal = asal;
+        }
+
+        public Mahasiswa()
+        {
+
+        }
+        
+        //method
+        public void SetMahasiswa(string nama, string nim)
+        {
+            this.nama = nama;
+            this.nim = nim;
+            Console.WriteLine("nama mahasiswa : (0)", this.nama);
+            Console.WriteLine("nama mahasiswa : (0)", this.nim);
+            Console.ReadKey();
+        }
+
+        public void SetAsal(string setAsal)
+        {
+            this.asal = asal;
+            Console.WriteLine("asal mahasiswa dari : {0}", this.asal);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Constructor/Constructor/Program.cs
@@ -0,0 +1,28 @@
+﻿using Constructor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace llll
+{
+    internal class Program
+    {
+        static void Main(string[] agrs)
+        {
+            Mahasiswa mahasiswa1 = new Mahasiswa("Kevin","22.11.4870","Kaltim");
+            Console.WriteLine(mahasiswa1.nama + "\n" + mahasiswa1.nim + "\n" + mahasiswa1.asal);
+            Console.WriteLine("-----------------------------------");
+
+
+            Mahasiswa mahasiswa2 = new Mahasiswa();
+            mahasiswa2.SetMahasiswa("Kevin","08888888");
+            mahasiswa2.SetAsal("Kaltim");
+            Console.WriteLine(mahasiswa2);
+
+            Console.ReadKey();
+        }
+    
+    }
+}
