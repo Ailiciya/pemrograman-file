775af4ca9a00a026e66366d063052bb708cd01c1
Kevin <114798157+KevinKyun@users.noreply.github.com>
2023-06-04 23:08:55 +0700

    Add files via upload

---
+++ Inheritance/Inheritance/Person.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+using System;
+
+namespace Inheritance
+{
+    public class Person
+    {
+        // Properties
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        // Constructor
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        // Method
+        public void InfoPerson()
+        {
+            Console.WriteLine("Nama saya {0} dan umur saya {1} tahun", Name, Age);
+            Console.ReadLine();
+        }
+    }
+
+    // Turunan dari class Person
+    public class Karyawan : Person
+    {
+        // Properties karyawan
+        public string KaryawanID { get; set; }
+        public string Subject { get; set; }
+
+        // Constructor
+        public Karyawan(string name, int age, string karyawanID) : base(name, age)
+        {
+            KaryawanID = karyawanID;
+        }
+
+        // Override method
+        public new void InfoPerson()
+        {
+            Console.WriteLine("Nama saya {0}, umur saya {1} tahun, dan ID karyawan saya {2}", Name, Age, KaryawanID);
+            Console.ReadLine();
+        }
+    }
+
+}
---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    static class Program
+    {
+        static void Main()
+        {
+            // Membuat objek Person menggunakan konstruktor
+            Person person1 = new Person("Kevin", 19);
+            person1.InfoPerson();
+
+            // Membuat objek Karyawan menggunakan konstruktor
+            Karyawan karyawan1 = new Karyawan("Kokomi", 20, "01");
+            karyawan1.InfoPerson();
+        }
+    }
+}
