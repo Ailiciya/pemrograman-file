dc8016dd25d974a42578a4d0dcd64ace5ed0b266
Kevin <114798157+KevinKyun@users.noreply.github.com>
2023-06-06 14:46:37 +0700

    Add files via upload

---
+++ Polymorphism/Polymorphism/Animal.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    internal class Animal
+    {
+        public class Hewan
+        {
+            public string Name { get; set; }
+            public virtual void speak()
+            {
+                Console.WriteLine("semua hewann bersuara");
+            }
+        }
+
+        public class Cat : Animal
+        {
+            public void Bersuara()
+            {
+                Console.WriteLine("kucing bersuara : meong");
+            }
+        }
+
+        public class Cow : Animal
+        {
+            public void Bersuara()
+            {
+                Console.WriteLine("sapi bersuara : mooooo");
+            }
+        }
+        public class Aligator : Animal
+        {
+            public void Bersuara()
+            {
+                Console.WriteLine("buaya bersuara : hay manizz");
+            }
+        }
+
+        public class Chicken : Animal
+        {
+            public void Bersuara()
+            {
+                Console.WriteLine("ayam bersuara : kukuruyuk");
+            }
+        }
+
+    }
+}
---
+++ Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal.Cat cat = new Animal.Cat();
+            cat.Bersuara();
+
+            Animal.Cow cow = new Animal.Cow();
+            cow.Bersuara();
+
+            Animal.Aligator aligator = new Animal.Aligator();
+            aligator.Bersuara();
+
+            Animal.Chicken chicken = new Animal.Chicken();
+            chicken.Bersuara();
+
+            Console.ReadLine();
+        }
+    }
+}
